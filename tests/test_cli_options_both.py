e1d9557b85eda4edabdc39bb882ab26394635a6b
Istvan Sarandi <prog@istvansarandi.com>
2026-01-08 20:42:19 +0100

    Add oracle tests for CLI and RC file options
    - test_cli_options_both.py: short/long options, -D/-S/-R, simulate,
      no-folding, dotfiles, bundled options, verbose levels
    - test_rc_options_both.py: ~/.stowrc, .stowrc precedence, defer/ignore
      in rc files, CLI overrides

---
+++ tests/test_cli_options_both.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+#
+# This file is part of GNU Stow.
+#
+# GNU Stow is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GNU Stow is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see https://www.gnu.org/licenses/.
+
+"""
+Black-box oracle tests for CLI option parsing.
+Tests both Perl and Python implementations via CLI, verifying:
+1. Each implementation passes the original .t-style assertions
+2. Both implementations produce identical results
+
+Based on Perl t/cli_options.t
+"""
+
+import os
+
+from conftest import (
+    check_dir,
+    check_link,
+    check_not_exists,
+    run_both_tests,
+)
+
+
+class TestCliOptionsBoth:
+    """Test CLI options - black-box comparison of both implementations."""
+
+    def test_short_options(self, stow_env):
+        """Test short options -d, -t, -S."""
+        stow_env.create_package("pkg", {"bin/file": "content"})
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, "bin", "../stow/pkg/bin")
+
+        run_both_tests(
+            stow_env,
+            ["-d", stow_env.stow_dir, "-t", stow_env.target_dir, "-S", "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
+
+    def test_long_options(self, stow_env):
+        """Test long options --dir, --target, --stow."""
+        stow_env.create_package("pkg", {"bin/file": "content"})
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, "bin", "../stow/pkg/bin")
+
+        run_both_tests(
+            stow_env,
+            [f"--dir={stow_env.stow_dir}", f"--target={stow_env.target_dir}", "--stow", "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
+
+    def test_delete_option(self, stow_env):
+        """Test -D/--delete option."""
+        stow_env.create_package("pkg", {"bin/file": "content"})
+
+        def setup():
+            stow_env.run_perl_stow(["-t", stow_env.target_dir, "pkg"])
+
+        def check(env):
+            check_not_exists(env, "bin")
+
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "-D", "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
+
+    def test_restow_option(self, stow_env):
+        """Test -R/--restow option."""
+        stow_env.create_package("pkg", {"bin/file": "content"})
+
+        def setup():
+            stow_env.run_perl_stow(["-t", stow_env.target_dir, "pkg"])
+            # Add new file to package
+            with open(os.path.join(stow_env.stow_dir, "pkg", "bin", "file2"), "w") as f:
+                f.write("content2")
+
+        def check(env):
+            # After restow, bin remains a folder symlink (both files in same package)
+            check_link(env, "bin", "../stow/pkg/bin")
+
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "-R", "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
+
+    def test_simulate_option(self, stow_env):
+        """Test -n/--simulate option."""
+        stow_env.create_package("pkg", {"bin/file": "content"})
+
+        def setup():
+            pass
+
+        def check(env):
+            # Nothing should be created in simulate mode
+            check_not_exists(env, "bin")
+
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "-n", "pkg"],
+            setup,
+            check,
+            check_on_simulate=True,
+            compare_fs_ops=False,  # No FS operations in simulate mode
+        )
+
+    def test_no_folding_option(self, stow_env):
+        """Test --no-folding option."""
+        stow_env.create_package(
+            "pkg",
+            {
+                "dir/file1": "content1",
+                "dir/file2": "content2",
+            },
+        )
+
+        def setup():
+            pass
+
+        def check(env):
+            check_dir(env, "dir")
+            check_link(env, "dir/file1", "../../stow/pkg/dir/file1")
+            check_link(env, "dir/file2", "../../stow/pkg/dir/file2")
+
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "--no-folding", "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
+
+    def test_dotfiles_option(self, stow_env):
+        """Test --dotfiles option."""
+        stow_env.create_package(
+            "pkg",
+            {
+                "dot-bashrc": "# bashrc",
+                "dot-config/app/settings": "settings",
+            },
+        )
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, ".bashrc", "../stow/pkg/dot-bashrc")
+            check_link(env, ".config", "../stow/pkg/dot-config")
+
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "--dotfiles", "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
+
+    def test_bundled_short_options(self, stow_env):
+        """Test bundled short options like -nvS."""
+        stow_env.create_package("pkg", {"file": "content"})
+
+        def setup():
+            pass
+
+        def check(env):
+            # Simulate mode, nothing created
+            check_not_exists(env, "file")
+
+        # -nvS means: simulate, verbose, stow
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "-nS", "pkg"],
+            setup,
+            check,
+            check_on_simulate=True,
+            compare_fs_ops=False,
+        )
+
+    def test_verbose_levels(self, stow_env):
+        """Test multiple verbose flags."""
+        stow_env.create_package("pkg", {"file": "content"})
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, "file", "../stow/pkg/file")
+
+        # Multiple -v flags
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "-v", "-v", "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
+
+    def test_mixed_stow_unstow_restow(self, stow_env):
+        """Test mixing -D, -S, -R for different packages."""
+        stow_env.create_package("pkg_delete", {"bin/delete": "content"})
+        stow_env.create_package("pkg_stow", {"bin/stow": "content"})
+        stow_env.create_package("pkg_restow", {"bin/restow": "content"})
+
+        def setup():
+            # Pre-stow pkg_delete and pkg_restow
+            stow_env.run_perl_stow(["-t", stow_env.target_dir, "pkg_delete"])
+            stow_env.run_perl_stow(["-t", stow_env.target_dir, "pkg_restow"])
+
+        def check(env):
+            # pkg_delete should be gone
+            check_not_exists(env, "bin/delete")
+            # pkg_stow should be stowed
+            check_link(env, "bin/stow", "../../stow/pkg_stow/bin/stow")
+            # pkg_restow should be re-stowed
+            check_link(env, "bin/restow", "../../stow/pkg_restow/bin/restow")
+
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "-D", "pkg_delete", "-S", "pkg_stow", "-R", "pkg_restow"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=True,
+        )
---
+++ tests/test_rc_options_both.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+#
+# This file is part of GNU Stow.
+#
+# GNU Stow is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GNU Stow is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see https://www.gnu.org/licenses/.
+
+"""
+Black-box oracle tests for .stowrc file handling.
+Tests both Perl and Python implementations via CLI, verifying:
+1. Each implementation passes the original .t-style assertions
+2. Both implementations produce identical results
+
+Based on Perl t/rc_options.t
+"""
+
+import os
+
+from conftest import (
+    check_link,
+    run_both_tests,
+)
+
+
+class TestRcOptionsBoth:
+    """Test .stowrc handling - black-box comparison of both implementations."""
+
+    def test_home_stowrc_paths(self, stow_env):
+        """Test that ~/.stowrc --dir and --target are used."""
+        stow_env.create_package("pkg", {"bin/file": "content"})
+
+        # Create ~/.stowrc with dir and target
+        with open(os.path.join(stow_env.tmpdir, ".stowrc"), "w") as f:
+            f.write(f"-d {stow_env.stow_dir}\n")
+            f.write(f"--target={stow_env.target_dir}\n")
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, "bin", "../stow/pkg/bin")
+
+        # Note: run from stow_dir (where .stowrc -d points)
+        # The stowrc provides --dir and --target, so just pass package name
+        # Don't compare fs_ops - RC file handling has known syscall differences
+        run_both_tests(
+            stow_env,
+            ["pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=False,
+        )
+
+    def test_stowrc_defer_option(self, stow_env):
+        """Test --defer in .stowrc."""
+        stow_env.create_package("pkg1", {"man/file": "first"})
+        stow_env.create_package("pkg2", {"man/file": "second"})
+
+        # Create ~/.stowrc with defer
+        with open(os.path.join(stow_env.tmpdir, ".stowrc"), "w") as f:
+            f.write(f"-d {stow_env.stow_dir}\n")
+            f.write(f"--target={stow_env.target_dir}\n")
+            f.write("--defer=man\n")
+
+        def setup():
+            stow_env.create_target_dir("man")
+            stow_env.run_perl_stow(["-t", stow_env.target_dir, "pkg1"])
+
+        def check(env):
+            # pkg1's file should remain (pkg2 deferred via rc)
+            check_link(env, "man/file", "../../stow/pkg1/man/file")
+
+        run_both_tests(
+            stow_env,
+            ["pkg2"],
+            setup,
+            check,
+            check_on_simulate=True,
+            compare_fs_ops=False,
+        )
+
+    def test_stowrc_ignore_option(self, stow_env):
+        """Test --ignore in .stowrc."""
+        stow_env.create_package(
+            "pkg",
+            {
+                "bin/keep": "keep",
+                "bin/skip.bak": "skip",
+            },
+        )
+
+        # Create ~/.stowrc with ignore
+        with open(os.path.join(stow_env.tmpdir, ".stowrc"), "w") as f:
+            f.write(f"-d {stow_env.stow_dir}\n")
+            f.write(f"--target={stow_env.target_dir}\n")
+            f.write("--ignore=\\.bak\n")
+
+        def setup():
+            stow_env.create_target_dir("bin")
+
+        def check(env):
+            check_link(env, "bin/keep", "../../stow/pkg/bin/keep")
+            assert not os.path.exists(os.path.join(env.target_dir, "bin", "skip.bak"))
+
+        run_both_tests(
+            stow_env,
+            ["pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=False,
+        )
+
+    def test_cwd_stowrc_overrides_home(self, stow_env):
+        """Test that .stowrc in cwd overrides ~/.stowrc."""
+        stow_env.create_package("pkg", {"bin/file": "content"})
+
+        # Create ~/.stowrc with wrong target
+        with open(os.path.join(stow_env.tmpdir, ".stowrc"), "w") as f:
+            f.write(f"-d {stow_env.stow_dir}\n")
+            f.write(f"--target=/nonexistent/should/be/overridden\n")
+
+        # Create .stowrc in stow_dir (cwd) with correct target
+        with open(os.path.join(stow_env.stow_dir, ".stowrc"), "w") as f:
+            f.write(f"--target={stow_env.target_dir}\n")
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, "bin", "../stow/pkg/bin")
+
+        run_both_tests(
+            stow_env,
+            ["pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=False,
+        )
+
+    def test_cli_overrides_stowrc(self, stow_env):
+        """Test that CLI options override .stowrc."""
+        stow_env.create_package("pkg", {"file": "content"})
+
+        # Wrong target in stowrc
+        wrong_target = os.path.join(stow_env.tmpdir, "wrong_target")
+        os.makedirs(wrong_target)
+        with open(os.path.join(stow_env.tmpdir, ".stowrc"), "w") as f:
+            f.write(f"-d {stow_env.stow_dir}\n")
+            f.write(f"--target={wrong_target}\n")
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, "file", "../stow/pkg/file")
+
+        # CLI --target should override stowrc
+        run_both_tests(
+            stow_env,
+            ["-t", stow_env.target_dir, "pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=False,
+        )
+
+    def test_stowrc_with_verbose(self, stow_env):
+        """Test verbose option in .stowrc."""
+        stow_env.create_package("pkg", {"file": "content"})
+
+        with open(os.path.join(stow_env.tmpdir, ".stowrc"), "w") as f:
+            f.write(f"-d {stow_env.stow_dir}\n")
+            f.write(f"--target={stow_env.target_dir}\n")
+            f.write("-v\n")
+
+        def setup():
+            pass
+
+        def check(env):
+            check_link(env, "file", "../stow/pkg/file")
+
+        run_both_tests(
+            stow_env,
+            ["pkg"],
+            setup,
+            check,
+            check_on_simulate=False,
+            compare_fs_ops=False,
+        )
